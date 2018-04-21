5d2b9236eae44f1640082c45c5639b4ee39845b3
samepant <sampanter@gmail.com>
2018-04-21 01:18:06 -0400

    adds manifesto and venn cursor

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 function showManifesto () {
   console.log('here for the manifesto')
 }
+
+AFRAME.registerComponent('show-manifesto', {
+  schema: {
+    on: {type: 'string'}
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+    el.addEventListener('click', function () {
+      console.log('it happened')
+    });
+  }
+});
+
+AFRAME.registerComponent('my-alpha-test', {
+  dependencies: ['material'],
+
+  init: function () {
+    var material = this.el.getObject3D('mesh').material;
+    material.alphaTest = 0.5;
+    material.needsUpdate = true;
+  }
+});
