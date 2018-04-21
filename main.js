f061e83fdb7d5151034a1f2c4c7d6e12e4d31dc5
samepant <sampanter@gmail.com>
2018-04-21 08:49:20 -0400

    loading screen

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var scene = document.querySelector('a-scene');
+    var splash = document.querySelector('#splash');
+    scene.addEventListener('loaded', function (e) {
+        splash.style.display = 'none';
+    });
+}); 
+
 function showManifesto () {
   console.log('here for the manifesto')
 }
